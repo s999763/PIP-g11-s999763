cbf1cffbfd8f576e0b6983a2a2d2a5317ab28074
graba12 <115491821+graba12@users.noreply.github.com>
2022-12-03 19:23:42 +0100

    Update Main.java
    fix multiplication in 4,5

--- g11 s999763 20.11.22/src/Main.java
+++ g11 s999763 20.11.22/src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+
+
 public class Main {
     public static void main (String[] args) {
         //=========ZADANIE 1========
@@ -10,14 +13,11 @@ public class Main {
 
         if (czyPada == true && czySwieciSlonce == false) {
             System.out.println("Plucha");
-        }
-        else if (czyPada == true && czySwieciSlonce == true) {
+        } else if (czyPada == true && czySwieciSlonce == true) {
             System.out.println("Tęcza");
-        }
-        else if (czyPada == false && czySwieciSlonce == true) {
+        } else if (czyPada == false && czySwieciSlonce == true) {
             System.out.println("Słonecznie");
-        }
-        else if (czyPada == false && czySwieciSlonce == false) {
+        } else if (czyPada == false && czySwieciSlonce == false) {
             System.out.println("Pochmurno");
         }
         //======KONIEC ZADANIA 1======
@@ -31,17 +31,17 @@ public class Main {
         System.out.println("Podaj typ działania:");
         String dzialanie = scan.nextLine();
 
-        switch(dzialanie) {
+        switch (dzialanie) {
             case "ADD":
-                int addResult = pierwszaLiczba+drugaLiczba;
+                int addResult = pierwszaLiczba + drugaLiczba;
                 System.out.println("Wynik: " + absoluteValue(addResult));
                 break;
-            case  "SUB":
-                int subResult = pierwszaLiczba-drugaLiczba;
+            case "SUB":
+                int subResult = pierwszaLiczba - drugaLiczba;
                 System.out.println("Wynik: " + absoluteValue(subResult));
                 break;
             case "DIV":
-                int divResult =  pierwszaLiczba / drugaLiczba;
+                int divResult = pierwszaLiczba / drugaLiczba;
                 System.out.println("Wynik: " + absoluteValue(divResult));
                 break;
             case "MUL":
@@ -55,56 +55,55 @@ public class Main {
         //======KONIEC ZADANIA 4,5========
 
 
-
         //======ZADANIE 6=======
         System.out.println("Wprowadź 2 liczby rzeczywiste i naciśnij ENTER po każdej z nich:");
         float a = scan.nextFloat();
         float b = scan.nextFloat();
-        if (a>b){
+        if (a > b) {
             float temp = a;
             a = b;
-            b = temp;}
-        System.out.println("Wybrany przedział: ["+ a + ", " + b + "]");
+            b = temp;
+        }
+        System.out.println("Wybrany przedział: [" + a + ", " + b + "]");
         Random r = new Random();
-        float d1 = r.nextFloat(b-a+1)+a;
-        float d2 = r.nextFloat(b-a+1)+a;
-        float d3 = r.nextFloat(b-a+1)+a;
+        float d1 = r.nextFloat(b - a + 1) + a;
+        float d2 = r.nextFloat(b - a + 1) + a;
+        float d3 = r.nextFloat(b - a + 1) + a;
 
         System.out.println("wartości generowane losowo:" + d1 + "; " + d2 + "; " + d3);
 
         //jak ustawić liczby w kolejności
-        if (d1<d2 && d1<d3 && d2<d3){
+        if (d1 < d2 && d1 < d3 && d2 < d3) {
 
-        System.out.println("Gdzie: "+d1+"<"+d2+"<"+d3);}
-        else if (d1<d2 && d1<d3 && d3<d2) {
-            System.out.println("Gdzie: "+d1+"<"+d3+"<"+d2);}
-        else if (d1>d2 && d1<d3 && d2<d3) {
-            System.out.println("Gdzie: "+d2+"<"+d1+"<"+d3);}
-        else if (d3<d1 && d3<d2 && d1<d2){
-            System.out.println("Gdzie: "+d3+"<"+d1+"<"+d2);}
-        else if (d2<d3 && d2<d1 && d3<d1){
-            System.out.println("Gdzie: "+d2+"<"+d3+"<"+d1);}
-        else if (d3<d2 && d3<d1 && d2<d1){
-            System.out.println("Gdzie: "+d3+"<"+d2+"<"+d1);}
+            System.out.println("Gdzie: " + d1 + "<" + d2 + "<" + d3);
+        } else if (d1 < d2 && d1 < d3 && d3 < d2) {
+            System.out.println("Gdzie: " + d1 + "<" + d3 + "<" + d2);
+        } else if (d1 > d2 && d1 < d3 && d2 < d3) {
+            System.out.println("Gdzie: " + d2 + "<" + d1 + "<" + d3);
+        } else if (d3 < d1 && d3 < d2 && d1 < d2) {
+            System.out.println("Gdzie: " + d3 + "<" + d1 + "<" + d2);
+        } else if (d2 < d3 && d2 < d1 && d3 < d1) {
+            System.out.println("Gdzie: " + d2 + "<" + d3 + "<" + d1);
+        } else if (d3 < d2 && d3 < d1 && d2 < d1) {
+            System.out.println("Gdzie: " + d3 + "<" + d2 + "<" + d1);
+        }
         //======KONIEC ZADANIA 6========
         //======ZADANIE 8=======
         System.out.println("Witamy, wybierz walutę: 1 - PLN; 2 - JPY");
-        int waluta= scan.nextInt();
+        int waluta = scan.nextInt();
         System.out.println("Wprowadź kwotę:");
         int kwota = scan.nextInt();
-        if (waluta==1){
-            System.out.println(kwota + "zł => "+ kwota*30.29 + "¥");
-        }
-        else if (waluta==2){
-            System.out.println(kwota + "¥ => " + kwota*0.03 + "zł");
-        }
-        else {
+        if (waluta == 1) {
+            System.out.println(kwota + "zł => " + kwota * 30.29 + "¥");
+        } else if (waluta == 2) {
+            System.out.println(kwota + "¥ => " + kwota * 0.03 + "zł");
+        } else {
             System.out.println("Nieprawidłowa wartość, rozpocznij ponownie");
         }
 
         //======KONIEC ZADANIA 8========
-        }
-
+    }
+    
 
     //metoda licząca wartość bezwzględną
     public static int absoluteValue(int k) {
